4ee67626d0b2826c2a880b764549426e88593316
widiarsaf <wretasafitri33@gmail.com>
2025-11-15 23:53:29 +0700

    Insert transaksi_bus and transaksi_halte to postgreSQL

--- dags/etl_transaction_data_dag.py
+++ dags/etl_transaction_data_dag.py
@@ -23,23 +23,23 @@ def extract_csv_files():
         "shelter_corridor" : shelter_corridor.to_json()
     }
 
-# def insert_transaksiBus_to_postgres():
-#     df = pd.read_csv(DATA_PATH + "dummy_transaksi_bus.csv")
-#     pg = PostgresHook(postgres_conn_id="postgres_default")
+def insert_transaksiBus_to_postgres():
+    df = pd.read_csv(DATA_PATH + "dummy_transaksi_bus.csv")
+    pg = PostgresHook(postgres_conn_id="postgres_default")
 
-#     engine = pg.get_sqlalchemy_engine()
+    engine = pg.get_sqlalchemy_engine()
 
-#     df.to_sql("dummy_transaksi_bus", engine, if_exists="replace", index=False)
-#     print("dummy_transaksi_bus inserted into PostgreSQL")
+    df.to_sql("dummy_transaksi_bus", engine, if_exists="replace", index=False)
+    print("dummy_transaksi_bus inserted into PostgreSQL")
 
-# def insert_transaksiHalte_to_postgres():
-#     df = pd.read_csv(DATA_PATH + "dummy_transaksi_halte.csv")
-#     pg = PostgresHook(postgres_conn_id="postgres_default")
+def insert_transaksiHalte_to_postgres():
+    df = pd.read_csv(DATA_PATH + "dummy_transaksi_halte.csv")
+    pg = PostgresHook(postgres_conn_id="postgres_default")
 
-#     engine = pg.get_sqlalchemy_engine()
+    engine = pg.get_sqlalchemy_engine()
 
-#     df.to_sql("dummy_transaksi_halte", engine, if_exists="replace", index=False)
-#     print("dummy_transaksi_halte inserted into PostgreSQL")
+    df.to_sql("dummy_transaksi_halte", engine, if_exists="replace", index=False)
+    print("dummy_transaksi_halte inserted into PostgreSQL")
 
 
 def transform_function():
@@ -68,15 +68,15 @@ with DAG(
         python_callable=extract_csv_files
     )
 
-    # insert_transaksiBus = PythonOperator(
-    #     task_id="insert_transaksiBus_to_postgres",
-    #     python_callable=insert_transaksiBus_to_postgres
-    # )
+    insert_transaksiBus = PythonOperator(
+        task_id="insert_transaksiBus_to_postgres",
+        python_callable=insert_transaksiBus_to_postgres
+    )
 
-    # insert_transaksiHalte = PythonOperator(
-    #     task_id="insert_transaksiHalte_to_postgres",
-    #     python_callable=insert_transaksiHalte_to_postgres
-    # )
+    insert_transaksiHalte = PythonOperator(
+        task_id="insert_transaksiHalte_to_postgres",
+        python_callable=insert_transaksiHalte_to_postgres
+    )
 
 
     # Transform Data
@@ -92,4 +92,4 @@ with DAG(
         python_callable=load_function
     )
 
-    extract_data >> transform_data >> load_data
+    extract_data >> [insert_transaksiBus, insert_transaksiHalte] >> transform_data >> load_data
