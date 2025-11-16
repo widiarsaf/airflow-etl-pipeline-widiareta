7b6ad2f02d512eae827195a6441dc044fd807f7f
widiarsaf <wretasafitri33@gmail.com>
2025-11-16 17:22:35 +0700

    Update transformation process: insert fact_transactions table to postgres

--- dags/etl_transaction_data_dag.py
+++ dags/etl_transaction_data_dag.py
@@ -47,6 +47,7 @@ def extract_all_data_sources(ti):
 def transform_data(ti):
     df_transaksi_bus = pd.read_json(ti.xcom_pull(key="transaksi_bus"))
     df_transaksi_halte = pd.read_json(ti.xcom_pull(key="transaksi_halte"))
+
     df_transaksi_halte["no_body_var"] = None
 
     df = pd.concat([df_transaksi_bus, df_transaksi_halte], ignore_index=True)
@@ -54,50 +55,32 @@ def transform_data(ti):
 
     def standardize_no_body(val):
         if pd.isna(val) or str(val).strip() == "":
-            return None  # Halte transactions → No bus body → Leave null
+            return None
 
         val = str(val).replace("_", "-").replace(" ", "-")
         parts = val.split("-")
 
-        # If value is only "BRT" or "TJ" without number
         if len(parts) < 2:
             return f"{parts[0]}-000"
 
         prefix = parts[0]
         digits = ''.join([x for x in parts[1] if x.isdigit()])
         digits = digits.zfill(3)
-
         return f"{prefix}-{digits}"
-    
+
     df['no_body_var'] = df["no_body_var"].astype(str).apply(standardize_no_body)
+
+
     df["tanggal"] = pd.to_datetime(df["waktu_transaksi"]).dt.date
     df["is_pelanggan"] = df["status_var"].apply(lambda x: 1 if x == "S" else 0)
     df["amount"] = df["fare_int"]
 
-    # Aggregation by cards
-    agg_by_card = df.groupby(
-        ["tanggal", "card_type_var", "gate_in_boo"]
-    ).agg(
-        pelanggan=("is_pelanggan", "sum"),     
-        amount=("amount", "sum")               
-    ).reset_index()
-
-    # Aggregation by tarif
-    agg_by_tarif = df.groupby(
-        ["tanggal", "fare_int", "gate_in_boo"]
-    ).agg(
-        pelanggan=("is_pelanggan", "sum"),
-        amount=("amount", "sum")
-    ).reset_index()
-    agg_by_tarif = agg_by_tarif.rename(columns={"fare_int": "tarif"})
-
-    # Aggregation by routes
     df_routes = pd.read_json(ti.xcom_pull(key="routes"))
     df_realisasi = pd.read_json(ti.xcom_pull(key="realisasi_bus"))
 
     df_realisasi["bus_body_no"] = df_realisasi["bus_body_no"].astype(str).apply(standardize_no_body)
 
-    df_route_join = df.merge(
+    df = df.merge(
         df_realisasi,
         how="left",
         left_on="no_body_var",
@@ -105,37 +88,54 @@ def transform_data(ti):
     )
 
     possible_route_cols = ["route_code_var", "route_code", "rute_realisasi"]
-
     route_col_found = None
+
     for col in possible_route_cols:
-        if col in df_route_join.columns:
+        if col in df.columns:
             route_col_found = col
             break
 
     if not route_col_found:
-        raise Exception(f"❌ No route code column found in merged data. Columns: {df_route_join.columns.tolist()}")
+        raise Exception(f"❌ No route code column found. Available columns: {df.columns.tolist()}")
 
-    df_route_join = df_route_join.merge(
+    # Merge ke tabel routes
+    df = df.merge(
         df_routes,
         left_on=route_col_found,
         right_on="route_code",
         how="left"
     )
+    ti.xcom_push("fact_transaction", df.to_json())
 
 
-    agg_by_route = df_route_join.groupby(
-        ["tanggal", "route_code", "route_name", "gate_in_boo"]
+    agg_by_card = df.groupby(
+        ["tanggal", "card_type_var", "gate_in_boo"]
     ).agg(
         pelanggan=("is_pelanggan", "sum"),
         amount=("amount", "sum")
     ).reset_index()
 
 
+    agg_by_tarif = df.groupby(
+        ["tanggal", "fare_int", "gate_in_boo"]
+    ).agg(
+        pelanggan=("is_pelanggan", "sum"),
+        amount=("amount", "sum")
+    ).reset_index().rename(columns={"fare_int": "tarif"})
+
+
+    agg_by_route = df.groupby(
+        ["tanggal", "route_code", "route_name", "gate_in_boo"]
+    ).agg(
+        pelanggan=("is_pelanggan", "sum"),
+        amount=("amount", "sum")
+    ).reset_index()
+
     ti.xcom_push("agg_by_card", agg_by_card.to_json())
     ti.xcom_push("agg_by_tarif", agg_by_tarif.to_json())
     ti.xcom_push("agg_by_route", agg_by_route.to_json())
-    
-    print("Tranforming and aggregating data completed")
+
+    print("Transforming and aggregating data completed")
 
 
 def load_function():
