7684cbf3c163b8290c409b6817b14f1e033f6fd9
widiarsaf <wretasafitri33@gmail.com>
2025-11-16 10:53:00 +0700

    Update load data to pg and extracting data process

--- dags/etl_transaction_data_dag.py
+++ dags/etl_transaction_data_dag.py
@@ -1,45 +1,47 @@
 import pandas as pd
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from datetime import datetime
 
 DATA_PATH = "/opt/airflow/data/input/"
+OUTPUT_PATH = "/opt/airflow/data/output/"
 
-def extract_csv_files():
-    print("Reading all csv files")
 
-    transaksi_bus = pd.read_csv(DATA_PATH + "dummy_transaksi_bus.csv")
-    transaksi_halte = pd.read_csv(DATA_PATH + "dummy_transaksi_halte.csv")
-    routes = pd.read_csv(DATA_PATH + "dummy_routes.csv")
-    realisasi_bus = pd.read_csv(DATA_PATH + "dummy_realisasi_bus.csv")
-    shelter_corridor = pd.read_csv(DATA_PATH + "dummy_shelter_corridor.csv")
+def load_csv_to_postgres():
+    df_transaksi_bus = pd.read_csv(DATA_PATH + "dummy_transaksi_bus.csv")
+    df_transaksi_halte = pd.read_csv(DATA_PATH + "dummy_transaksi_halte.csv")
 
-    return {
-        "transaksi_bus" : transaksi_bus.to_json(),
-        "transaksi_halte" : transaksi_halte.to_json(),
-        "routes" : routes.to_json(),
-        "realisasi_bus" : realisasi_bus.to_json(),
-        "shelter_corridor" : shelter_corridor.to_json()
-    }
-
-def insert_transaksiBus_to_postgres():
-    df = pd.read_csv(DATA_PATH + "dummy_transaksi_bus.csv")
     pg = PostgresHook(postgres_conn_id="postgres_default")
-
     engine = pg.get_sqlalchemy_engine()
 
-    df.to_sql("dummy_transaksi_bus", engine, if_exists="replace", index=False)
-    print("dummy_transaksi_bus inserted into PostgreSQL")
+    df_transaksi_bus.to_sql("dummy_transaksi_bus", engine, if_exists="replace", index=False)
+    df_transaksi_halte.to_sql("dummy_transaksi_halte", engine, if_exists="replace", index=False)
+    print("Insert data into PostgreSQL completed")
 
-def insert_transaksiHalte_to_postgres():
-    df = pd.read_csv(DATA_PATH + "dummy_transaksi_halte.csv")
+
+    
+def extract_all_data_sources(ti):
+    df_routes = pd.read_csv(DATA_PATH + "dummy_routes.csv")
+    df_realisasi_bus = pd.read_csv(DATA_PATH + "dummy_realisasi_bus.csv")
+    df_shelter_corridor = pd.read_csv(DATA_PATH + "dummy_shelter_corridor.csv")
+
     pg = PostgresHook(postgres_conn_id="postgres_default")
+    conn = pg.get_conn()
 
-    engine = pg.get_sqlalchemy_engine()
+    df_transaksi_bus = pd.read_sql("SELECT * FROM dummy_transaksi_bus", conn)
+    df_transaksi_halte = pd.read_sql("SELECT * FROM dummy_transaksi_halte", conn)
 
-    df.to_sql("dummy_transaksi_halte", engine, if_exists="replace", index=False)
-    print("dummy_transaksi_halte inserted into PostgreSQL")
+    # Send all to Transform via XCom
+    ti.xcom_push(key="routes", value=df_routes.to_json())
+    ti.xcom_push(key="realisasi_bus", value=df_realisasi_bus.to_json())
+    ti.xcom_push(key="shelter_corridor", value=df_shelter_corridor.to_json())
+    ti.xcom_push(key="transaksi_bus", value=df_transaksi_bus.to_json())
+    ti.xcom_push(key="transaksi_halte", value=df_transaksi_halte.to_json())
+
+    print("Extracting data sources completed")
+    
 
 
 def transform_function():
@@ -49,11 +51,6 @@ def load_function():
     print("Loading data...")
 
 
-
-
-
-
-
 with DAG(
     dag_id="etl_transaction_data_dag",
     start_date=datetime(2024, 1, 1),
@@ -61,24 +58,24 @@ with DAG(
     catchup=False,
     tags=["ETL", "extract"]
 ) as dag:
+    start = EmptyOperator(
+        task_id="start"
+    )
 
     # Extract Data: read csv, insert to postgreSQL
-    extract_data = PythonOperator(
-        task_id="extract_csv_files",
-        python_callable=extract_csv_files
+
+    load_csv_to_postgres = PythonOperator(
+        task_id="load_csv_to_postgres",
+        python_callable=load_csv_to_postgres
     )
 
-    insert_transaksiBus = PythonOperator(
-        task_id="insert_transaksiBus_to_postgres",
-        python_callable=insert_transaksiBus_to_postgres
+    extract_all_data_sources = PythonOperator(
+        task_id="extract_all_data_sources",
+        python_callable=extract_all_data_sources,
+        provide_context=True
     )
 
-    insert_transaksiHalte = PythonOperator(
-        task_id="insert_transaksiHalte_to_postgres",
-        python_callable=insert_transaksiHalte_to_postgres
-    )
-
-
+    
     # Transform Data
     transform_data = PythonOperator(
         task_id="transform_data",
@@ -92,4 +89,10 @@ with DAG(
         python_callable=load_function
     )
 
-    extract_data >> [insert_transaksiBus, insert_transaksiHalte] >> transform_data >> load_data
+
+    end = EmptyOperator(
+            task_id="end"
+        )
+
+
+    start >>  load_csv_to_postgres >> extract_all_data_sources >> transform_data >> load_data >> end
